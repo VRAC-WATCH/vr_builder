308089ea5ef1f1afbf95917934b5dfc0fdc1feb2
Trevor Richardson <richardson.trev@gmail.com>
2014-05-06 16:57:45 -0500

    Added setup methods and an additional constructor for InteractionManager

--- include/InteractionManager.h
+++ include/InteractionManager.h
@@ -1,19 +1,48 @@
 // System headers
-#include <stdarg.h>
-#include <iostream>
-#ifdef WIN32
-#include <windows.h>
-#endif
+#include <vector>
 
-// GL headers
-#ifdef __APPLE__
-#  include <GLUT/glut.h>
-#else
-#  include <GL/glut.h>
-#endif
+// Forward declarations
+class Input;
 
-class InteractionManager {
+class InteractionManager
+{
+public:
+	
+	/** Enum for declaring the Interface type. */
+	enum InterfaceType {
+		GLUT_INTERFACE,
+		JUGGLER_INTERFACE
+	};
+	
+	/** Default Constructor - Sets up the glut interface. */
 	InteractionManager();
+	
+	/** Constructor declaring interface. */
+	InteractionManager(InterfaceType type);
+	
+	/** Destructor. */
+	~InteractionManager();
+	
+	/** Accessors for the specified input type. */
+	Input* input();
+		
+	
+	/** 
+	 * Builds a list of commands that have been executed from any input. 
+	 * This needs to be done every frame before trying to use commands.
+	 */
 	void update();
-
+	
+	
+protected:
+	
+	/** Sets up user interaction handlers for Glut. */
+	void _setupGlutInputs();
+	
+	/** Sets up user interaction hanlders for Juggler. */
+	void _setupJugglerInputs();
+	
+	/** iVars. */
+	std::vector<Input*>		_inputs;
+	InterfaceType			_interfaceType;
 };
--- src/InteractionManager.cpp
+++ src/InteractionManager.cpp
@@ -1,9 +1,44 @@
+// System headers
+#include <iostream>
+
+// Local headers
 #include "InteractionManager.h"
+#include "KeyboardInput.h"
 
-InteractionManager::InteractionManager() {
+InteractionManager::InteractionManager()
+{
+	_setupGlutInputs();
+}
 
-	}
+InteractionManager::InteractionManager(InterfaceType type)
+{
+	if (JUGGLER_INTERFACE)
+		_setupGlutInputs();
+	else
+		_setupJugglerInputs();
+}
 
-void InteractionManager::update() {
+InteractionManager::~InteractionManager()
+{
+	std::cout << "InteractionManager Destructor" << std::endl;
+}
 
+void InteractionManager::update()
+{
+	std::cout << "Updating InteractionManager" << std::endl;
+}
+
+#pragma mark Class Setup
+
+void InteractionManager::_setupGlutInputs()
+{
+	std::cout << "Setting up glut input" << std::endl;
+	
+	// Setup the keyboard
+//	Input* keyboard ;
+}
+
+void InteractionManager::_setupJugglerInputs()
+{
+	std::cout << "Setting up juggler input" << std::endl;
 }
