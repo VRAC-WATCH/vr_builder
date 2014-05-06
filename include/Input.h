4c9e0b39916084eca7f4698e8d6ad96e2630288d
Trevor Richardson <richardson.trev@gmail.com>
2014-05-06 16:59:44 -0500

    Added base implementation of the Input class
    This will eventually be the pure virtual class for all Inputs that InputManager holds onto.

---
+++ include/Input.h
@@ -0,0 +1,23 @@
+//
+//  Input.h
+//  VRBuilder
+//
+//  Created by Trevor Richardson on 5/6/14.
+//
+//
+
+#ifndef __VRBuilder__Input__
+#define __VRBuilder__Input__
+
+class Input
+{
+public:
+	
+	/** Default Constructor. */
+	Input();
+	
+	/** Destructor. */
+	~Input();
+};
+
+#endif /* defined(__VRBuilder__Input__) */
---
+++ src/Input.cpp
@@ -0,0 +1,23 @@
+//
+//  Input.cpp
+//  VRBuilder
+//
+//  Created by Trevor Richardson on 5/6/14.
+//
+//
+
+// System headers
+#include <iostream>
+
+// Local headers
+#include "Input.h"
+
+Input::Input()
+{
+	std::cout << "Input Constructor" << std::endl;
+}
+
+Input::~Input()
+{
+	std::cout << "Input Desctructor" << std::endl;
+}
