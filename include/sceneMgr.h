985112867ff567507c1a6f2ad3d11a91fbff328d
Frederick Thompson <fvt@iastate.edu>
2014-05-06 21:44:56 -0500

    Adding to scene manager
    started declaring/defining the scene manager, very rough draft
    
    Signed-off-by: Frederick Thompson <fvt@iastate.edu>

--- include/sceneMgr.h
+++ include/sceneMgr.h
@@ -11,4 +11,16 @@
 
 #include <iostream>
 
+
+
+//Own the scene
+//Use the builder - send updates
+//Take commands from App manager
+    //initialize default scene
+    //interaction command update
+//Update scene - send updates from builder
+//Get information from the scene
+
+
+
 #endif /* defined(__VRBuilder__sceneMgr__) */
--- src/sceneMgr.cpp
+++ src/sceneMgr.cpp
@@ -7,3 +7,54 @@
 //
 
 #include "sceneMgr.h"
+//#include "scene.h" //uncomment when scene is available
+//
+
+//declarations
+
+class sceneManager
+{
+    //private members
+    
+    //an instance of the scene (not really a string)
+    std::string instanceScene;
+    
+    public:
+    
+    //reads in an input from the Application Manager
+    void listenAppManger(std::string);
+    
+    //sends an update from builder to the scene
+    std::string builderToScene(void);//using strings for now
+    
+    //listens to the scene
+    void listenScene( std::string );//using strings for now
+    
+    //An instance of some data from the scene
+    void sceneInstance(void);
+};
+
+
+//definitions
+
+
+    void sceneManager::listenAppManger(std::string command)
+    {
+        //"directions from the app manager" = command;
+    };
+
+    std::string BuilderToScene(void)
+    {
+        //return "directions to the scene"
+        return ("hello");
+    };
+
+    void sceneManager::listenScene(std::string command)
+    {
+        //"directions from the app manager" = command;
+    };
+
+    void sceneManager::sceneInstance(void)
+    {
+     //   Scene instanceScene = new Scene();
+    }
