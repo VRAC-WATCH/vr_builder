f743d24909fae6fae8003aa15d37cde0f02e4fcc
Trevor Richardson <richardson.trev@gmail.com>
2014-05-07 08:28:30 -0500

    InteractionManager now cleans up all inputs that it created during startup in its destructor.

--- src/InteractionManager.cpp
+++ src/InteractionManager.cpp
@@ -4,6 +4,7 @@
 // Local headers
 #include "InteractionManager.h"
 #include "KeyboardInput.h"
+#include "SceneCommand.h"
 
 InteractionManager::InteractionManager()
 {
@@ -21,11 +22,20 @@ InteractionManager::InteractionManager(InterfaceType type)
 InteractionManager::~InteractionManager()
 {
 	std::cout << "InteractionManager Destructor" << std::endl;
+	
+	for (unsigned int i=0; i<_inputs.size(); ++i) {
+		delete _inputs.at(i);
+	}
 }
 
 void InteractionManager::update()
 {
 	std::cout << "Updating InteractionManager" << std::endl;
+	
+	std::vector<SceneCommand> command_list;
+	for (unsigned int i=0; i<_inputs.size(); ++i) {
+		_inputs.at(i)->populateSceneCommand(command_list);
+	}
 }
 
 #pragma mark Class Setup
@@ -35,7 +45,11 @@ void InteractionManager::_setupGlutInputs()
 	std::cout << "Setting up glut input" << std::endl;
 	
 	// Setup the keyboard
-//	Input* keyboard ;
+	Input* keyboard = new KeyboardInput;
+	_inputs.push_back(keyboard);
+	
+	// Setup the mouse
+	
 }
 
 void InteractionManager::_setupJugglerInputs()
