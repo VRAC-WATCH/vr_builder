c5845b99cbfd154e5f80cdb3900c443a94800d42
Frederick Thompson <fvt@iastate.edu>
2014-05-08 23:03:36 -0500

    more work on the scene manager
    I'm having problems with SceneCommand.h, I think it's not finding an OSG file and v3 isn't working for me.
    
    Signed-off-by: Frederick Thompson <fvt@iastate.edu>

--- include/sceneMgr.h
+++ include/sceneMgr.h
@@ -22,5 +22,27 @@
 //Get information from the scene
 
 
+class sceneManager
+{
+    //private members
+    
+public:
+    
+    //reads in an input from the Application Manager
+    void listenAppManger(const struct SceneCommand&);
+    
+    //sends an update from builder to the scene
+    std::string builderToScene(void);//using strings for now
+    
+    //listens to the scene
+    void listenScene( std::string );//using strings for now
+    
+    //An instance of some data from the scene
+    void sceneInstance(void);
+    
+    //Intended to store the location of the blocks in the scene in progress
+    //may end up moving somewhere else
+    void storeBock(const struct SceneCommand&)
+};
 
 #endif /* defined(__VRBuilder__sceneMgr__) */
--- src/sceneMgr.cpp
+++ src/sceneMgr.cpp
@@ -8,70 +8,25 @@
 
 #include "sceneMgr.h"
 //#include "builder.h"
-//include "SceneCommand.h"
+#include "SceneCommand.h"
 //#include "scene.h" //uncomment when scene is available
 
-
-
-//declarations
-
-
-//These will eventually go in the .h, I'm just keeping them here for simplicity!
-class sceneManager
-{
-    //private members
     
     //an instance of the scene (not really a string)
     std::string instanceScene;
     
-    public:
-    
     //reads in an input from the Application Manager
-    void listenAppManger(const struct SceneCommand&);
-    
-    //sends an update from builder to the scene
-    std::string builderToScene(void);//using strings for now
-    
-    //listens to the scene
-    void listenScene( std::string );//using strings for now
-    
-    //An instance of some data from the scene
-    void sceneInstance(void);
-};
-
-//Intended to store the location of the blocks in the scene in progress
-//may end up moving somewhere else
-
-class currentScene
-{
-    void storeBlock(const struct SceneCommand&);
-};
-
-//definitions
-
-//commented temporarily
-
-/*    void sceneManager::listenAppManger(const struct scenecommand&)
+    void sceneManager::listenAppManger(const SceneCommand scenecommand&)
     {
-        //SceneCommand scenecommand;
-        //mModelGroup->addChild(Builder::instance().createBlock(scenecommand));
-    };
-*/
+    //SceneCommand scenecommand;
+    //mModelGroup->addChild(Builder::instance().createBlock(scenecommand));
+    }
 
-///////////////////
-//I don't think builder will be passing info back to scenemanager, correct?
-
-    /*std::string BuilderToScene(void)
-    {
-        //return "directions to the scene"
-        return ("hello");
-    };*/
-///////////////////
 
     void sceneManager::listenScene(std::string command)
     {
         //"directions from the app manager" = command;
-    };
+    }
 
     void sceneManager::sceneInstance(void)
     {
